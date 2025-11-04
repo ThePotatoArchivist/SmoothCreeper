b4d325a28de6192e32e9c37f90f5967f5c30fcfa
ThePotatoArchivist <josiahfu@gmail.com>
2025-11-04 01:49:29 -0800

    1.21.1

--- src/main/java/archives/tater/smoothcreeper/mixin/BlockBehaviourMixin.java
+++ src/main/java/archives/tater/smoothcreeper/mixin/BlockBehaviourMixin.java
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Slice;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
 @Mixin(BlockBehaviour.class)
@@ -18,13 +18,13 @@ public class BlockBehaviourMixin {
     @ModifyArg(
             method = "onExplosionHit",
             slice = @Slice(
-                    from = @At(value = "FIELD", target = "Lnet/minecraft/world/level/storage/loot/parameters/LootContextParams;TOOL:Lnet/minecraft/util/context/ContextKey;"),
-                    to = @At(value = "FIELD", target = "Lnet/minecraft/world/level/storage/loot/parameters/LootContextParams;BLOCK_ENTITY:Lnet/minecraft/util/context/ContextKey;")
+                    from = @At(value = "FIELD", target = "Lnet/minecraft/world/level/storage/loot/parameters/LootContextParams;TOOL:Lnet/minecraft/world/level/storage/loot/parameters/LootContextParam;"),
+                    to = @At(value = "FIELD", target = "Lnet/minecraft/world/level/storage/loot/parameters/LootContextParams;BLOCK_ENTITY:Lnet/minecraft/world/level/storage/loot/parameters/LootContextParam;")
             ),
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/loot/LootParams$Builder;withParameter(Lnet/minecraft/util/context/ContextKey;Ljava/lang/Object;)Lnet/minecraft/world/level/storage/loot/LootParams$Builder;"),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/loot/LootParams$Builder;withParameter(Lnet/minecraft/world/level/storage/loot/parameters/LootContextParam;Ljava/lang/Object;)Lnet/minecraft/world/level/storage/loot/LootParams$Builder;"),
             index = 1
     )
-    private Object changeTool(Object object, @Local(argsOnly = true) ServerLevel level, @Local(argsOnly = true) Explosion explosion) {
+    private Object changeTool(Object object, @Local(argsOnly = true) Level level, @Local(argsOnly = true) Explosion explosion) {
         return explosion.getDirectSourceEntity() instanceof Creeper ? SmoothCreeper.TOOL.apply(level) : object;
     }
 }
